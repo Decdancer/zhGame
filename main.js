e39bfe3dc01e1b54699bc352b847046d84994cff
“Zhanna“ <maekot@googlemail.com>
2021-06-17 17:35:21 +0200

    gameStep function +  Tireset game + timeOut

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ quizC1.shuffleColors()
 
 
 
-// insert a new color-name to look for
+// Test - insert a new color-name to look for
 const newWhat = document.querySelector("#quiz-title > span")
 console.log(newWhat.innerText)
 newWhat.innerText = 'tomato'
@@ -46,7 +46,7 @@ newWhat.innerText = quizColors[1].name
 
 
 
-// insert a new color-name into card
+// Test - insert a new color-name into card
 const newValue = document.querySelector('.cards div')
 console.log(newValue)
 
@@ -55,13 +55,21 @@ const newBox = document.querySelector('.cards')
 document.querySelector('#card1').value="baby"
 console.log(document.querySelector('#card1'))
 
-//insert a new pic
+// Test - insert a new pic -> not relevant anymore - just colors-game
 const newPic = document.querySelector('img').setAttribute('src', 'https://images.unsplash.com/photo-1552757158-788e68d046dc?')
 
+// Test - set a new color
+const newCard11 = document.querySelector('#card1')
+newCard11.style.backgroundColor = quizColors[2].bcolor;
+newCard11.value = 'bb'
+console.log(newCard11.style.backgroundColor)
+console.log(quizColors[5].bcolor)
+console.log(newCard11)
+console.log(newCard11.value)
 
-//game - to see a word & guess the correct pic out of 3
+// Game - to see a word & guess the correct pic out of 3
 
-//1) random choice of 3 objects colors(pics)-words out of source
+//1) random choice of 3 objects colors(pics)-words from the source
 function shuffleColors(arr) {
     let newArr = [];
     let arr1 = arr.slice()
@@ -79,14 +87,72 @@ console.log(shuffleColors(quizColors));
 
 console.log(shuffleColors(quizColors));
 
-//2) random (not anymore because of the shuffle before) placement of those three colors(pics) on three cards-places
-const newCard11 = document.querySelector('#card1')
-newCard11.style.backgroundColor = quizColors[2].bcolor;
-newCard11.value = 'bb'
-console.log(newCard11.style.backgroundColor)
-console.log(quizColors[5].bcolor)
-console.log(newCard11)
-console.log(newCard11.value)
+
+//2) random (no need in random anymore because of the shuffle before) placement of those three colors(pics) on three cards-places
+
+/*
+document.querySelectorAll('.cards > div').forEach(function(card) {
+    console.log(card);
+    card.load = gameStep;
+    card.onclick = checkCorrect; 
+})
+
+function gameStep(card) {
+    card.style.borderColor = '#999999';
+    let randomArr = shuffleColors(quizColors);
+    let name2Guess = document.querySelector("#quiz-title > span");
+    let random3 = Math.floor(Math.random() * 3);
+    console.log(randomArr)
+
+    for (let i = 0; i <= arr.length; i++) {
+
+        card.style.backgroundColor += randomArr[i].bcolor;
+        card.value += randomArr[i].name;
+
+        name2Guess.innerText = randomArr[random3].name;
+
+        console.log(random3)
+        console.log(randomArr[random3].name)
+    };
+}
+*/
+function resetGame() {
+document.querySelectorAll('.cards > div').forEach(function(card) { 
+    card.style.borderColor = '#999999';
+})
+
+let newArr8 = shuffleColors(quizColors)
+console.log(newArr8)
+
+newCard1 = document.querySelector('#card1')
+newCard1.style.backgroundColor = newArr8[0].bcolor;
+newCard1.value = newArr8[0].name;
+//console.log(newCard1)
+//console.log(newCard1.value)
+
+newCard2 = document.querySelector('#card2')
+newCard2.style.backgroundColor = newArr8[1].bcolor;
+newCard2.value = newArr8[1].name;
+//console.log(newCard2)
+//console.log(newCard2.value)
+
+    newCard3 = document.querySelector('#card3')
+    newCard3.style.backgroundColor = newArr8[2].bcolor;
+    newCard3.value = newArr8[2].name;
+    //console.log(newCard3)
+    //console.log(newCard3.value)
+
+
+    //3) random choice of 1 word from those 3 to display as quiz-word
+
+    newWhat1 = document.querySelector("#quiz-title > span")
+    let random3 = Math.floor(Math.random() * 3);
+    newWhat1.innerText = newArr8[random3].name
+    console.log(random3)
+    console.log(newArr8[random3].name)
+    }
+
+//
 
 let newArr2 = shuffleColors(quizColors)
 console.log(newArr2)
@@ -94,21 +160,23 @@ console.log(newArr2)
 let newCard1 = document.querySelector('#card1')
 newCard1.style.backgroundColor = newArr2[0].bcolor;
 newCard1.value = newArr2[0].name;
-console.log(newCard1)
-console.log(newCard1.value)
+//console.log(newCard1)
+//console.log(newCard1.value)
 
 let newCard2 = document.querySelector('#card2')
 newCard2.style.backgroundColor = newArr2[1].bcolor;
 newCard2.value = newArr2[1].name;
-console.log(newCard2)
-console.log(newCard2.value)
+//console.log(newCard2)
+//console.log(newCard2.value)
 
 let newCard3 = document.querySelector('#card3')
 newCard3.style.backgroundColor = newArr2[2].bcolor;
 newCard3.value = newArr2[2].name;
-console.log(newCard3)
-console.log(newCard3.value)
-//3) random choice of 1 word from those three to display as quiz-word
+//console.log(newCard3)
+//console.log(newCard3.value)
+
+
+//3) random choice of 1 word from those 3 to display as quiz-word
 
 let newWhat1 = document.querySelector("#quiz-title > span")
 let random3 = Math.floor(Math.random() * 3);
@@ -116,26 +184,14 @@ newWhat1.innerText = newArr2[random3].name
 console.log(random3)
 console.log(newArr2[random3].name)
 
-// user clickson a card
-/* 
-1) if html has : <div id="card1" onclick="clickFunction()">
-2) document.querySelector('#card1').onclick = clickFunction; or = function () {console.log('click');}
 
-function clickFunction() {
-console.log('click');
-}
-*/
+//4) user clicks on a card to check if the guess is correct
 
-/*
-document.querySelector('#card1').addEventListener('click', clickFunction1);
-function clickFunction1() {
-    console.log('click');
-}
-*/
 
 document.querySelectorAll('.cards > div').forEach(function(card) {
     card.onclick = checkCorrect;  
 })
+
 
 function checkCorrect(event) {
  console.log(event.target.value);
@@ -146,26 +202,71 @@ function checkCorrect(event) {
  document.querySelector('#all-attempts').innerText = countAtt.innerText++
     console.log(countAtt)
 
-
- if (event.target.value !== newWhat1.innerText) {
+console.log(document.querySelector("#quiz-title > span").innerText)
+ if (event.target.value !== document.querySelector("#quiz-title > span").innerText) {
     event.target.style.borderColor = 'red';
-    //event.target.onclick('checked');
+    //event.target.onclick('checked'); -> not working
     console.log('no711');
     }        
-else {
+ else {
     event.target.style.borderColor = 'green';
-    //event.target.onclick('good');
+    //event.target.onclick('good'); -> not working
     console.log('yes2');
 
     countSuccess = document.querySelector('#correct-attempts');
     countSuccess.innerText++
     document.querySelector('#correct-attempts').innerText = countSuccess.innerText++
     console.log(countAtt);
-    return shuffleColors(quizColors)
+
+    //let newArr333 = shuffleColors(quizColors);
+    //return newArr333;
+    // return shuffleColors(quizColors) --> not working
+    // plus function - set initia bordes, shuffle
+    setTimeout(resetGame, 3000)
     }
 }
 
 /*
+    document.querySelectorAll('.cards > div').forEach(function(card) { 
+        card.style.borderColor = '#999999';
+    })
+
+    let newArr8 = shuffleColors(quizColors)
+console.log(newArr8)
+
+ newCard1 = document.querySelector('#card1')
+newCard1.style.backgroundColor = newArr8[0].bcolor;
+newCard1.value = newArr8[0].name;
+//console.log(newCard1)
+//console.log(newCard1.value)
+
+ newCard2 = document.querySelector('#card2')
+newCard2.style.backgroundColor = newArr8[1].bcolor;
+newCard2.value = newArr8[1].name;
+//console.log(newCard2)
+//console.log(newCard2.value)
+
+ newCard3 = document.querySelector('#card3')
+newCard3.style.backgroundColor = newArr8[2].bcolor;
+newCard3.value = newArr8[2].name;
+//console.log(newCard3)
+//console.log(newCard3.value)
+
+
+//3) random choice of 1 word from those 3 to display as quiz-word
+
+ newWhat1 = document.querySelector("#quiz-title > span")
+let random3 = Math.floor(Math.random() * 3);
+newWhat1.innerText = newArr8[random3].name
+console.log(random3)
+console.log(newArr8[random3].name)
+    }
+    */
+
+
+
+/* Test - clickFunction
+
 function clickFunction1() {
     let cardThis = document.querySelector('#card1')
     counterAtt = document.querySelector('#all-attempts')
@@ -180,8 +281,6 @@ function clickFunction1() {
         console.log('yes2');
         return shuffleColors(quizColors)}
 }
-
-
 
 document.querySelector('#card2').addEventListener('click', clickFunction2);
 function clickFunction2() {
@@ -210,16 +309,34 @@ function clickFunction3() {
         console.log('yes3');
         }
 }
-*/
 
-//4-right) if player clicks on the correct pic -> its border turns green, score++ -> in a moment game step 1)
+
+//Test - 4-right) if player clicks on the correct pic -> its border turns green, score++ -> in a moment game step 1)
 if (newCard1.value === newWhat1.innerText) {console.log('yes1')}
 else if (newCard2.value === newWhat1.innerText) {console.log('yes2')}
 else if (newCard3.value === newWhat1.innerText) {console.log('yes3')}
 else {console.log('no3')};
 
-//4-wrong) not correct pic -> its border turns red
+//Test - 4-wrong) not correct pic -> its border turns red
 if (newCard1.value !== newWhat1.innerText) {console.log('no1')}
 else if (newCard2.value !== newWhat1.innerText) {console.log('no2')}
 else if (newCard3.value !== newWhat1.innerText) {console.log('no3')}
 else {console.log('no4')};
+*/
+
+/* Test - various ways to measure clicks
+
+1) if html has : <div id="card1" onclick="clickFunction()">
+2) document.querySelector('#card1').onclick = clickFunction; or = function () {console.log('click');}
+
+function clickFunction() {
+console.log('click');
+}
+*/
+
+/*
+document.querySelector('#card1').addEventListener('click', clickFunction1);
+function clickFunction1() {
+    console.log('click');
+}
+*/
